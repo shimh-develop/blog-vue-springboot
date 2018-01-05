9a880ea0291f3404593203ff0c206221e479c897
shimh-develop <91943154@qq.com>
2018-01-05 09:00:38 +0800

    引入ElementUI

--- blog-app/src/components/HelloWorld.vue
+++ blog-app/src/components/HelloWorld.vue
@@ -17,7 +17,15 @@
       <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
     </ul>
+    
+    
+  	<el-button type="primary" icon="el-icon-edit"></el-button>
+		<el-button type="primary" icon="el-icon-share"></el-button>
+		<el-button type="primary" icon="el-icon-delete"></el-button>
+		<el-button type="primary" icon="el-icon-search">搜索</el-button>
+		<el-button type="primary">上传<i class="el-icon-upload el-icon--right"></i></el-button>
   </div>
+  
 </template>
 
 <script>
--- blog-app/src/main.js
+++ blog-app/src/main.js
@@ -4,9 +4,15 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
+Vue.use(ElementUI)
+
+
+
 new Vue({
   el: '#app',
   router,
